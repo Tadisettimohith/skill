a5b14cdc4263e68560bc86169f7459fbe8a875d4
Tadisettimohith <muralitadisetti321@gmail.com>
2025-07-30 11:09:41 +0530

    Add files via upload

---
+++ Concatenatestring.java
@@ -0,0 +1,12 @@
+package skill;
+
+public class Concatenatestring {
+	 public static void main(String[] args) {
+	        String first = "Hello";
+	        String second = "World";
+	        String result = first + " " + second;
+	        System.out.println("Concatenated: " + result);
+	    }
+
+
+}
